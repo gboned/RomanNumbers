8afcea78c664aefe973a20913208975feeaaf29a
gboned <gboned@cifpfbmoll.eu>
2020-09-24 00:37:08 +0200

    test: Comprobar que el enum devuelve valor correcto

--- src/test/java/edu/elsmancs/RomanNumbersTest.java
+++ src/test/java/edu/elsmancs/RomanNumbersTest.java
@@ -1,6 +1,9 @@
 package edu.elsmancs;
 
-import static org.junit.Assert.assertTrue;
+import edu.elsmancs.RomanNumbers;
+import edu.elsmancs.RomanSymbols;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -10,11 +13,13 @@ import org.junit.Test;
 public class RomanNumbersTest 
 {
     /**
-     * Rigorous Test :-)
+     *  Check that the method to get the decimal value from the enum
+     *  works properly.
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void romanSymbolsTest()
     {
-        assertTrue( true );
+        RomanSymbols romanSymbols = RomanSymbols.C;
+        assertEquals( 100, romanSymbols.getDecimalValue() );
     }
 }
